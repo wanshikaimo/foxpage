48ccb52da285ac5835832a7b906d0210f00c7a79
yhdeng <yhdeng@trip.com>
2022-03-03 17:24:17 +0800

    fix: 🐛 Fixed component register bug
    ✅ Closes: #3

--- packages/foxpage-server/config/environments/debug.ts
+++ packages/foxpage-server/config/environments/debug.ts
@@ -13,10 +13,27 @@ export default {
   ], // Skip to verify the interface of the token
   mongodb: mongoConfig || 'mongodb://127.0.0.1:45201/test?retryWrites=false', // Database connection string
   locale: 'en', // Current language
-  plugins: [],
+  plugins: [
+    '@foxpage/foxpage-plugin-aws-s3',
+    '@foxpage/foxpage-plugin-ares',
+    '@foxpage/foxpage-plugin-unpkg',
+  ],
   allLocales: ['en-US', 'zh-HK', 'en-HK', 'ko-KR', 'ja-JP'], // Supported locales
-  storageConfig: {},
+  storageConfig: {
+    bucket: 'foxpage',
+    config: {
+      region: 'ap-southeast-1',
+      credentials: {
+        accessKeyId: 'AKIASBTKBMEZFDKETTFX',
+        secretAccessKey: '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',
+      },
+    },
+  },
   resourceConfig: {
+    ares: {
+      registry: 'http://registry.ares.fws.qa.nt.ctripcorp.com',
+      static: 'http://static.fws.qa.nt.ctripcorp.com',
+    },
     unpkg: {},
   },
 };
